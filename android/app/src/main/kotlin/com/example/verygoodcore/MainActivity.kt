8d4baa3fabfe4a020c17c236fa79453650528486
Jimmy Wang <jimmyw22@gmail.com>
2024-03-03 02:18:34 -0800

    very_good_cli:very_good create flutter_app global_citizen_game

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.global_citizen_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:global_citizen_game/app/app.dart';
+import 'package:global_citizen_game/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
