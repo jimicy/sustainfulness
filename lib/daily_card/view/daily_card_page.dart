929bb7aaac070a825aed21eb69719cc9c699059d
Jimmy Wang <jimmyw22@gmail.com>
2024-03-12 07:01:28 -0700

    Add flutter_tilt and parallax tilt card.

--- lib/daily_card/view/daily_card_page.dart
+++ lib/daily_card/view/daily_card_page.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_tilt/flutter_tilt.dart';
 import 'package:global_citizen_game/app/revealed_card/cubit/daily_element_cubit.dart';
 import 'package:global_citizen_game/share/google_wallet/wallet_app.dart';
 import 'package:global_citizen_game/share/social_media/social_media_app.dart';
@@ -11,12 +14,6 @@ class DailyCardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('element ${context.read<DailyElementCubit>().state.element}');
-    print(
-        'elementAssetPath ${context.read<DailyElementCubit>().elementAssetPath}');
-    print(
-        'elementBgAssetPath ${context.read<DailyElementCubit>().elementBgAssetPath}');
-
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -53,13 +50,16 @@ class DailyCardPage extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(
-            height: 10,
+          const SizedBox(
+            height: 15,
           ),
-          Image(
-            height: 530,
-            image:
-                NetworkImage(context.read<DailyElementCubit>().state.cardUri),
+          AnimationCard(
+            child: Image(
+              filterQuality: FilterQuality.high,
+              height: 500,
+              image:
+                  NetworkImage(context.read<DailyElementCubit>().state.cardUri),
+            ),
           ),
           RevealedElementAnimation(
             elementAssetPath:
@@ -122,6 +122,106 @@ class RevealedElement extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class AnimationCard extends StatefulWidget {
+  const AnimationCard({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<AnimationCard> createState() => _AnimationCardState();
+}
+
+class _AnimationCardState extends State<AnimationCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController scaleAnimationController;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    scaleAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 350),
+    );
+    scaleAnimation = Tween(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(
+        parent: scaleAnimationController,
+        curve: Curves.easeOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    scaleAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Listener(
+      onPointerDown: (_) => scaleAnimationController.forward(),
+      onPointerUp: (_) => scaleAnimationController.reverse(),
+      onPointerCancel: (_) => scaleAnimationController.reverse(),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: (_) => scaleAnimationController.forward(),
+        onExit: (_) => scaleAnimationController.reverse(),
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          alignment: Alignment.center,
+          filterQuality: FilterQuality.high,
+          child: GestureDetector(
+            onTap: () => _showDialog(context),
+            // Tilt here
+            child: Tilt(
+              borderRadius: BorderRadius.circular(20),
+              tiltConfig: const TiltConfig(filterQuality: FilterQuality.high),
+              lightConfig: const LightConfig(
+                minIntensity: 0.1,
+                maxIntensity: 0.4,
+              ),
+              child: widget.child,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Align(
+            child: Transform.scale(
+              scale: 1.2,
+              filterQuality: FilterQuality.high,
+              // Tilt here
+              child: Tilt(
+                borderRadius: BorderRadius.circular(20),
+                tiltConfig: const TiltConfig(
+                  enableRevert: false,
+                  enableSensorRevert: false,
+                  filterQuality: FilterQuality.high,
+                ),
+                lightConfig: const LightConfig(
+                  minIntensity: 0.1,
+                  maxIntensity: 0.4,
+                ),
+                shadowConfig: const ShadowConfig(disable: true),
+                child: widget.child,
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
