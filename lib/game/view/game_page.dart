fd3f5fee2eb86eb8f59d1b4923c78324b2a0bb94
Jimmy Wang <jimmyw22@gmail.com>
2024-03-12 01:57:56 -0700

    Add a loading spinner for the Webview.

--- lib/game/view/game_page.dart
+++ lib/game/view/game_page.dart
@@ -11,6 +11,7 @@ class GameWebView extends StatefulWidget {
 
 class _GameWebViewState extends State<GameWebView> {
   late final WebViewController controller;
+  bool _loading = true;
 
   @override
   void initState() {
@@ -21,6 +22,10 @@ class _GameWebViewState extends State<GameWebView> {
         Uri.parse(
             'https://568f7e56-4141-44a9-bcc4-0c7b7d6a88c4-00-ab9ubil012q4.spock.replit.dev/'),
       )
+      ..setNavigationDelegate(NavigationDelegate(
+          onPageFinished: (_) => setState(() {
+                _loading = false;
+              })))
       ..addJavaScriptChannel("messageHandler",
           onMessageReceived: (JavaScriptMessage javaScriptMessage) {
         if (javaScriptMessage.message == "game-finished") {
@@ -39,8 +44,40 @@ class _GameWebViewState extends State<GameWebView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(
-        controller: controller,
+      backgroundColor: Color.fromRGBO(78, 109, 127, 1),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/splash2_bg.jpg',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          WebViewWidget(controller: controller),
+          if (_loading)
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Loading Game...',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w100,
+                        color: Colors.white,
+                      )),
+                  SizedBox(height: 10),
+                  CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            )
+        ],
       ),
     );
   }
